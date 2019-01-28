a02180363be2da40c5079e4665404d8a803599a4
wendellrodrigues <wendell.rodrigues@sjsu.edu>
2019-01-27 23:01:03 -0800

    Update Two-Way Bet Schema to clean it up/experiment with different types

--- models/TwoWayBet.js
+++ models/TwoWayBet.js
@@ -1,41 +1,118 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Create Schema
+// Different types of user schemas, try to see if different ones work (ie firstUser, secondUser)
 const TwoWayBetSchema = new Schema({
+
   firstUser: {
-    type: Schema.Types.ObjectId,
-    ref: 'users'
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'users'
+    },
+    amount: {
+      type: Number,
+      required: true
+    },
+    accepted: {
+      type: Boolean,
+      default: false
+    }
   },
-  secondUser: {
-    type: Schema.Types.ObjectId,
-    ref: 'users'
-  },
-  firstUserAmount: {
-    type: Number,
-    required: true
-  },
-  secondUserAmount: {
-    type: Number,
-    required: true
-  },
+
+  secondUser: ({
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'users'
+    },
+    amount: {
+      type: Number,
+      required: true
+    },
+    accepted: {
+      type: Boolean,
+      default: false
+    }
+  }),
+
   totalAmount: {
     type: Number,
     required: true
   },
-  firstUserAccepted : {
-    type: Boolean,
-    default: false
-  },
-  secondUserAccepted : {
-    type: Boolean,
-    default: false
-  },
+
   date: {
     type: Date,
     default: Date.now
-  }
+  },
+
 });
 
 module.exports = User = mongoose.model('bets', TwoWayBetSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// // Create Schema
+// const TwoWayBetSchema = new Schema({
+//   firstUser: {
+//     type: Schema.Types.ObjectId,
+//     ref: 'users'
+//   },
+//   secondUser: {
+//     type: Schema.Types.ObjectId,
+//     ref: 'users'
+//   },
+//   firstUserAmount: {
+//     type: Number,
+//     required: true
+//   },
+//   secondUserAmount: {
+//     type: Number,
+//     required: true
+//   },
+//   totalAmount: {
+//     type: Number,
+//     required: true
+//   },
+//   firstUserAccepted : {
+//     type: Boolean,
+//     default: false
+//   },
+//   secondUserAccepted : {
+//     type: Boolean,
+//     default: false
+//   },
+//   date: {
+//     type: Date,
+//     default: Date.now
+//   },
+
+//   firstUser: ({
+//     User: {
+
+//     },
+//   })
+
+
+// });
+
+// module.exports = User = mongoose.model('bets', TwoWayBetSchema);
 
